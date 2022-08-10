9da712a0fc6e397758ec53781ce69fb410db7fb4
michiganventureclub <110775563+michiganventureclub@users.noreply.github.com>
2022-08-10 08:58:37 -0400

    Update workflow.js

--- src/sections/workflow.js
+++ src/sections/workflow.js
@@ -10,27 +10,21 @@ import ArrowEven from 'assets/arrowEven.svg';
 const data = [
   {
     id: 1,
-    title: 'INVESTMENT AND RESEARCH TEAM',
+    title: 'Board Team',
     text:
-      'Investment research team is to conduct research on the industry and specific start-up companies, and form personal works such as analysis reports and term sheets. After that, we can connect the research results of your colleagues in the investment research department with the fund and provide internship certificates. If the term sheet is adopted, it can provide a valuable experience from research to investment, as well as related financial incentives, to complete the following.',
+      'Board team is mainly composed of our two presidents and 6 Vice Presidents. The Board team is responsible for the overall management and direction of Michigan Venture Club. We work to ensure that the club is running smoothly and efficiently, and that all members are able to participate and enjoy their experience. The Board team is also responsible for communicating with the club\'s members and keeping them updated on the latest news and events.',
   },
   {
     id: 2,
     title: 'GENERAL MANAGEMENT TEAM',
     text:
-      'The general management department is responsible for the management of the whole club, the management of internal human resources, the connection with external investors, the operation of we-media publicity to the public etc.',
+      'Industry research and deal sourcing team is combined with around 10 groups focusing on different industries such as technology, biology, health care, consumer and web3. The industry research and deal sourcing team is responsible for conducting research on various industries and identifying potential deals that the student club can pursue. This team is essential in helping the club to stay up-to-date on industry trends and to identify new opportunities for the club to get involved in. The team works closely with the club\'s Executive Board to ensure that all research is aligned with the club\'s goals and objectives.',
   },
   {
     id: 3,
     title: 'EXPERT ADVISORY TEAM',
     text:
-      'The expert advisory panel is similar to independent directors, inviting industry seniors, including but not limited to alumni, investors, entrepreneurs,FA to join and provide mentoring and internship opportunities for club members. At the same time, members of the club will automatically join the expert advisory panel after graduating from the campus and becoming venture capitalists in the workplace, thus forming a cycle of continuity, passing on the fire from generation to generation, and contributing to the growth of UM venture capitalists.',
-  },
-  {
-    id: 4,
-    title: 'Cooperation Council',
-    text:
-      'Fill in...',
+      'The public relations and operations team is responsible for communicating with the club\'s members, the public, school department and other organizations. They work to promote Michigan Venture Club\'s activities and events, and to keep our club running smoothly. The team is made up of a VP, some managers, and several members.',
   },
 ];
 
